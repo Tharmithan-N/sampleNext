0d0ab911dd84b997b45ae31a9eedeed0add93d6f
Tharmithan N <kumartharmi98@gmail.com>
2023-07-25 19:52:16 +0530

    small changes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "static",
-    content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ]
+    output: "export",
+    
 }
 
 module.exports = nextConfig
